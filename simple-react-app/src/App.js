583cdd0115b3c559b51c38eb719ef900f102285f
Brian <wamwangimathenge@gmail.com>
2023-01-25 15:39:50 +0300

    Flashcard State Created

--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from './components/FlashcardList';
 
 
 function App() {
+
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <h1>Hello World</h1>
+    <FlashcardList flashcards={flashcards} />
   );
 }
+
+
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "What is 2 + 2?",
+    answer: "4",
+    options: [
+      "2",
+      "3",
+      "4",
+      "5"
+    ]
+  },
+  {
+    id: 1,
+    question: "Question 2",
+    answer: "Answer",
+    options: [
+      "Answer",
+      "Answer1",
+      "Answer2",
+      "Answer3"
+    ]
+  }
+]
 
 export default App;
