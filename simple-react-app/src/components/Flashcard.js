e136eb1935d39a129e760affcebf4a78b11178e2
Brian <wamwangimathenge@gmail.com>
2023-01-25 16:01:42 +0300

    Display Options

--- simple-react-app/src/components/Flashcard.js
+++ simple-react-app/src/components/Flashcard.js
@@ -6,8 +6,19 @@ export default function Flashcard({flashcard}) {
     const [flip, setFlip] = useState(false)
 
   return (
-    <div onClick={() => setFlip(!flip)}>
-        {flip ? flashcard.answer : flashcard.question}
+    <div 
+        classname={`card ${flip ? 'flip' : ''}`}
+        onClick={() => setFlip(!flip)}
+    >
+        <div classname="front">
+            {flashcard.question}
+            <div classname="flashcard-options">
+                {flashcard.options.map(option => {
+                    return <div className="flashcard-option">{option}</div>
+                })}
+            </div>
+        </div>
+        <div className="back">{flashcard.answer}</div>
     </div>
   )
 }
