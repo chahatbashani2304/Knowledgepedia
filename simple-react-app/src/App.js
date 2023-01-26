16016a917580c43bbca3f0b90d88413e59153617
Brian <wamwangimathenge@gmail.com>
2023-01-26 16:15:31 +0300

    API Connected, UseEffect

--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -1,11 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './components/FlashcardList';
 import './App.css';
+import axios from 'axios';
 
 
 function App() {
 
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+
+  useEffect(() => {
+    axios
+      .get("https://opentdb.com/api.php?amount=10")
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: questionItem.correct_answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+        console.log(res.data)
+      })
+
+  }, [])
+
+
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
     <FlashcardList flashcards={flashcards} />
   );
